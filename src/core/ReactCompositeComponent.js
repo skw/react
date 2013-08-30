222ad57e641985cbb8ca8e7adeb93c310eb57b1d
Paul O'Shannessy <poshannessy@fb.com>
2013-08-29 17:32:53 -0700

    React.isValidClass
    Sometimes you may need to detect if a value is a valid React class constructor. This enables that and prevents future consumers from getting caught in the trap of depending on an internal implementation detail we might change.
    
    Currently this works for classes created with `React.createClass` as well as `React.DOM.*`.

--- src/core/ReactCompositeComponent.js
+++ src/core/ReactCompositeComponent.js
@@ -977,3 +977,16 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
