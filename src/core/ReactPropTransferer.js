skw/react

commit 3552e29db8da28c9a532b71f5c412e351c887871

Throw when calling transferPropsTo() on a component you don't own
This is dangerous because it means that data is flowing into the component from two components, only one of which is the actual "owner". While we may be able to figure out how to
support this someday, let's be strict and prevent it for now.

src/core/ReactPropTransferer.js
@@ -116,3 +116,11 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***

src/core/__tests__/ReactPropTransferer-test.js
@@ -131,3 +131,26 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***

commit b03fa56e7ec8e619e69d87535935e2a8ff2e1c62

Make transferPropsTo() message easier to debug
Summary:
Made the transferPropsTo() error introduced in
3552e29db8da28c9a532b71f5c412e351c887871 easier to use to debug.

src/core/ReactPropTransferer.js
@@ -124,3 +124,5 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***

src/core/__tests__/ReactPropTransferer-test.js
@@ -154,3 +154,4 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***