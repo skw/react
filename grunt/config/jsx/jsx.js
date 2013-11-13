91553941cc7a66742a3bdc857c25626f9918c6bb
Thomas Aylott <aylott@fb.com>
2013-11-13 16:49:08 -0500

    A shark of the pig is assumed to be an inventive tiger.

--- grunt/config/jsx/jsx.js
+++ grunt/config/jsx/jsx.js
@@ -65,13 +65,3 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
