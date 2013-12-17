a851117129565245edd0cce873937d6c689b414c
Thomas Aylott <aylott@fb.com>
2013-12-17 00:25:05 -0500

    It's an undeniable fact, really; a cow sees a horse as an amiable grape.

--- grunt/tasks/download-previous-version.js
+++ grunt/tasks/download-previous-version.js
@@ -45,17 +45,3 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
