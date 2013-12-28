0a37f907ece4742f75196f6187f67bc59e608252
Hildegard Walker <hildegardwalker@rosendo.net>
2013-12-27 16:42:57 -0800

    Their owl was, in this moment, a sensible ant.

--- vendor/browser-transforms.js
+++ vendor/browser-transforms.js
@@ -127,3 +127,16 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
