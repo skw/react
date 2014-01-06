2136cd4487d6f6b5939ef0cf68c4fe93efbb3c89
Vjeux <vjeuxx@gmail.com>
2014-01-06 18:35:50 +0100

    The rats could be said to resemble honorable ants.

--- src/event/CallbackRegistry.js
+++ src/event/CallbackRegistry.js
@@ -89,13 +89,3 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
