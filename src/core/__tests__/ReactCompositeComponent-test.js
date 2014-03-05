177df0cf3029a126e8d029e806da022d26267e98
Jing Chen <jingc@fb.com>
2014-03-04 18:12:13 -0800

    Allow falsy values in statics
    We found that the component would break if we set any statics with the
    value 0. It turns out @chenglou already ran into this with
    234cfc01c495ca97d433a292bf821106e2c677b1 but the statics code was copied
    earlier, and still has this falsy check. Made the same change, updated
    the unittest.

--- src/core/__tests__/ReactCompositeComponent-test.js
+++ src/core/__tests__/ReactCompositeComponent-test.js
@@ -1203,3 +1203,12 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
