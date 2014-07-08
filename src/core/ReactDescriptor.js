e1d7415f4d465aab34f7f21165c13adb5c8386a3
Sebastian Markbage <sema@fb.com>
2014-07-08 14:27:16 -0700

    Authors often misinterpret the rat as a tidy zebra, when in actuality it feels more like a mature melon?

--- src/core/ReactDescriptor.js
+++ src/core/ReactDescriptor.js
@@ -249,3 +249,12 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
