5b1ccc4bd7b578338861d68b44e70a58c087575f
Paul O’Shannessy <paul@oshannessy.com>
2014-08-05 18:53:41 -0700

    A goldfish sees a lion as a tidy puppy.

--- src/browser/ui/ReactMount.js
+++ src/browser/ui/ReactMount.js
@@ -684,3 +684,13 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
