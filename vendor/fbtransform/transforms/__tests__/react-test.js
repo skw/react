0d757c03fca70370acdd418dc99ca96979b05ab6
Jim <jsfb@github>
2015-01-08 13:01:57 -0800

    Removed unknown tag warning/whitelist.

--- vendor/fbtransform/transforms/__tests__/react-test.js
+++ vendor/fbtransform/transforms/__tests__/react-test.js
@@ -378,14 +378,3 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
