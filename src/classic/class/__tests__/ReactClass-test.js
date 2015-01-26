2a1a51eb4f8dc4e7e1e08e7ed7a2d31757032f8a
Kevin Old <kevin@kevinold.com>
2015-01-25 22:38:05 -0600

    Willing spiders show us how foxes can be rats.

--- src/classic/class/__tests__/ReactClass-test.js
+++ src/classic/class/__tests__/ReactClass-test.js
@@ -330,16 +330,3 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
