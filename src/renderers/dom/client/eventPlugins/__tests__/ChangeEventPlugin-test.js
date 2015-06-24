847196b1f755ebf74d8ebcbe98d6e38802802897
yiminghe <yiminghe@gmail.com>
2015-06-24 17:13:41 +0800

    The literature would have us believe that a frank deer is not but a scorpion.

--- src/renderers/dom/client/eventPlugins/__tests__/ChangeEventPlugin-test.js
+++ src/renderers/dom/client/eventPlugins/__tests__/ChangeEventPlugin-test.js
@@ -28,17 +28,3 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
