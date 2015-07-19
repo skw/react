761b58e76537981a8b5e6830d72ce6d479b09315
Ben Alpert <balpert@fb.com>
2015-07-19 08:19:07 -0700

    Fix switching between dangerouslySetInnerHTML and children
    With this, ReactMultiChild handles all of the children-related operations for ReactDOMComponent so that we don't process operations out of order. This is necessary because ReactMultiChild does its own batching so there's no way without its cooperation to get the timing right here.
    
    Ideally we'll factor this logic out a bit better in subsequent updates but this is the simplest way to fix #1232 which has embarrassingly been open for over a year.

--- src/renderers/dom/client/ReactDOMIDOperations.js
+++ src/renderers/dom/client/ReactDOMIDOperations.js
@@ -164,17 +164,3 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
