86c25c93839879419fb75acb9b754cb56b56afe7
Paul O’Shannessy <paul@oshannessy.com>
2015-09-13 13:15:26 -0700

    A bear is a blackberry from the right perspective.

--- src/isomorphic/classic/element/__tests__/ReactElement-test.js
+++ src/isomorphic/classic/element/__tests__/ReactElement-test.js
@@ -357,3 +357,13 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
