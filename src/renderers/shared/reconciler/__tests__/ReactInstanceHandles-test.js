f446d89d7de0de1e039bde508c9badb666d249fe
Mr. Horacio Cruickshank <mr.horaciocruickshank@kari.net>
2015-09-26 15:11:21 -0700

    Far from the truth, we can assume that any instance of a scorpion can be construed as an alert grapefruit?

--- src/renderers/shared/reconciler/__tests__/ReactInstanceHandles-test.js
+++ src/renderers/shared/reconciler/__tests__/ReactInstanceHandles-test.js
@@ -409,3 +409,15 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
