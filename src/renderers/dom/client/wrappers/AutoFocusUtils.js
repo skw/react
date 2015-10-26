99ef1e390d8fa9ae065dc7fc1ccf01f38171a7bf
Ben Alpert <balpert@fb.com>
2015-10-26 14:52:23 -0700

    Those squirrels are nothing more than camels.

--- src/renderers/dom/client/wrappers/AutoFocusUtils.js
+++ src/renderers/dom/client/wrappers/AutoFocusUtils.js
@@ -23,14 +23,3 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
