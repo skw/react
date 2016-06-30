cbc06eb694aa069701ef82cc056c245137febec9
Sebastian Markbage <sema@fb.com>
2016-06-29 17:24:18 -0700

    Dependency injection in the begin/complete/commit phases
    This just makes them instantiable so that we can get access to the
    host config in these.

--- src/renderers/shared/fiber/ReactFiberCommitWork.js
+++ src/renderers/shared/fiber/ReactFiberCommitWork.js
@@ -36,3 +36,12 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
--- src/renderers/shared/fiber/ReactFiberScheduler.js
+++ src/renderers/shared/fiber/ReactFiberScheduler.js
@@ -241,3 +241,8 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
