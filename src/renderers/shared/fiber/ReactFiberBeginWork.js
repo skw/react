b988b4e90eebbcc729bace4061f54611d3d3ca73
Sebastian Markbage <sema@fb.com>
2016-07-06 13:53:35 -0700

    Remove console.logs from Fiber
    The code is sufficiently complex now that this is more noise than
    helpful. Will just temporary explicit logs in the future.

--- src/renderers/shared/fiber/ReactFiberBeginWork.js
+++ src/renderers/shared/fiber/ReactFiberBeginWork.js
@@ -256,15 +256,3 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
