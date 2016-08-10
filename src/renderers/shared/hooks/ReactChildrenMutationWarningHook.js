182dd8b69778ba253cf3fb157a987d051afbf61d
Keyan Zhang <root@keyanzhang.com>
2016-08-11 02:50:18 +0800

    The first detailed elephant is, in its own way, a panda?

--- src/renderers/shared/hooks/ReactChildrenMutationWarningHook.js
+++ src/renderers/shared/hooks/ReactChildrenMutationWarningHook.js
@@ -54,13 +54,3 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
