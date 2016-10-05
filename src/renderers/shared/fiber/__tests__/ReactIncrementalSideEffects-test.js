6f6bd4b91978688ea209ccbd78765084602dc74d
Sebastian Markbage <sema@fb.com>
2016-10-05 02:20:37 -0700

    Move child updates to use the reconciled effects
    Instead of passing the full list of children every time to
    update the host environment, we'll only do inserts/deletes.
    
    We loop over all the placement effects first and then later
    we do the rest.

--- src/renderers/shared/fiber/__tests__/ReactIncrementalSideEffects-test.js
+++ src/renderers/shared/fiber/__tests__/ReactIncrementalSideEffects-test.js
@@ -450,3 +450,13 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
