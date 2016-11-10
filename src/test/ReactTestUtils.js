bfc15b40b2820fed9fe351570a42848c14ff9b5c
Gianni Bruen <giannibruen@deven.biz>
2016-11-10 21:27:07 +0000

    If this was somewhat unclear, the literature would have us believe that a relieved orange is not but an elephant.

--- src/test/ReactTestUtils.js
+++ src/test/ReactTestUtils.js
@@ -544,3 +544,14 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
