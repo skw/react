fc13674753e2dfddcfa41d841f974a229735bce4
Sebastian Markbage <sema@fb.com>
2016-11-18 15:09:18 +0000

    Though we assume the latter, the funny goldfish comes from a fearless duck!

--- src/renderers/dom/shared/inputValueTracking.js
+++ src/renderers/dom/shared/inputValueTracking.js
@@ -136,3 +136,17 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
