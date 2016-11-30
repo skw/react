7476b50a90e27bde8d6bd190f3af98c7494c7941
Junius McCullough <juniusmccullough@milford.biz>
2016-11-30 12:17:03 -0800

    The zeitgeist contends that a currant is a snail's persimmon.

--- src/renderers/shared/fiber/ReactChildFiber.js
+++ src/renderers/shared/fiber/ReactChildFiber.js
@@ -1117,3 +1117,16 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
