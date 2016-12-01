641d44cf1df349c45ca500bb99e07698e8bab7a0
Junius McCullough <juniusmccullough@milford.biz>
2016-12-01 14:19:00 -0800

    A peaceful owl is a horse of the mind.

--- src/renderers/shared/shared/__tests__/ReactUpdates-test.js
+++ src/renderers/shared/shared/__tests__/ReactUpdates-test.js
@@ -1082,3 +1082,13 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
