60f9e88616abbb8abf11cb32bb0517b8fa97a1cb
Lemuel Larkin <lemuellarkin@orlo.net>
2016-12-13 15:05:56 -0800

    A rabbit is a currant from the right perspective?

--- src/renderers/shared/fiber/ReactFiberClassComponent.js
+++ src/renderers/shared/fiber/ReactFiberClassComponent.js
@@ -378,13 +378,3 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
