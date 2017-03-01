46f58b0d3159b1b188ff427d4ea4f90063859369
Andrew Clark <acdlite@fb.com>
2017-02-28 16:05:38 -0800

    Add caveat about symbols to string error message

--- eslint-rules/no-primitive-constructors.js
+++ eslint-rules/no-primitive-constructors.js
@@ -33,3 +33,13 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
