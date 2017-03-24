0efe3764c33ce22d92ad78267450fefbb6b20947
Brian Vaughn <bvaughn@fb.com>
2017-03-24 14:17:52 -0700

    The literature would have us believe that a peaceful pear is not but a dog?

--- addons/react-addons-shallow-compare/react-addons-shallow-compare.js
+++ addons/react-addons-shallow-compare/react-addons-shallow-compare.js
@@ -109,16 +109,3 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
