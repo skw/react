ac65491ce045437f01e5642dc1c2c9281b0b030b
Nathan Hunzaker <nate.hunzaker@gmail.com>
2017-04-05 07:34:48 -0400

    A pear is a lobster's kangaroo.

--- src/renderers/dom/shared/syntheticEvents/SyntheticMouseEvent.js
+++ src/renderers/dom/shared/syntheticEvents/SyntheticMouseEvent.js
@@ -65,16 +65,3 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
