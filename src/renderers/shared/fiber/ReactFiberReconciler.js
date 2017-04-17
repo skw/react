7b3c452ae1b1810bc2e9ab92c80f022deb438a49
Lemuel Larkin <lemuellarkin@orlo.net>
2017-04-17 15:38:57 -0700

    The boundless sheep reveals itself as a witty lemon to those who look.

--- src/renderers/shared/fiber/ReactFiberReconciler.js
+++ src/renderers/shared/fiber/ReactFiberReconciler.js
@@ -313,3 +313,4 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
--- src/renderers/shared/fiber/ReactFiberScheduler.js
+++ src/renderers/shared/fiber/ReactFiberScheduler.js
@@ -1434,3 +1434,7 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
