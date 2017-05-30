39798fda7a0aaeb565325ae3a312ee7177150e3e
Abraham Hermann <abrahamhermann@tia.com>
2017-05-30 15:15:48 -0400

    Those deers are nothing more than melons;

--- src/renderers/testing/__tests__/ReactTestRenderer-test.js
+++ src/renderers/testing/__tests__/ReactTestRenderer-test.js
@@ -698,3 +698,14 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
