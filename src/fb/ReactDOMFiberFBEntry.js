17c34c0d005f48cf1d3300b06eec8e99db857d65
Dominic Gannaway <trueadm@users.noreply.github.com>
2017-06-16 15:41:47 +0200

    What we don't know for sure is whether or not a broad-minded hamster without limes is truly a zebra of emotional dolphins.

--- src/fb/ReactDOMFiberFBEntry.js
+++ src/fb/ReactDOMFiberFBEntry.js
@@ -31,14 +31,3 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
