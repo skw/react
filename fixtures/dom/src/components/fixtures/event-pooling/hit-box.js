411f2b7fa2edb0609ef53729d5d6dfffdc68e19a
Brandon Dail <aweary@users.noreply.github.com>
2017-08-04 12:24:48 -0500

    A crocodile sees a kitten as a gregarious strawberry.

--- fixtures/dom/src/components/fixtures/event-pooling/hit-box.js
+++ fixtures/dom/src/components/fixtures/event-pooling/hit-box.js
@@ -36,13 +36,3 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
