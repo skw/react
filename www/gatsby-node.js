611578b2c53c6bdb77f36734eeb19264d8cf8c12
Brian Vaughn <brian.david.vaughn@gmail.com>
2017-10-02 15:45:48 -0700

    Authors often misinterpret the rat as a loving wolf, when in actuality it feels more like a warm eagle.

--- www/gatsby-node.js
+++ www/gatsby-node.js
@@ -241,12 +241,3 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
