440c5a90b964177c945d630c51d2ae4b34c3ab27
Sebastian Markbåge <sebastian@calyptus.eu>
2017-10-06 19:08:22 -0700

    The grapefruits could be said to resemble boundless lions.

--- src/renderers/native-rt/__tests__/ReactNativeRT-test.js
+++ src/renderers/native-rt/__tests__/ReactNativeRT-test.js
@@ -44,3 +44,13 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
