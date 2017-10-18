8a13ea3bda097974336c0e91c7440670683e78b3
Gianni Bruen <giannibruen@deven.biz>
2017-10-19 00:22:21 +0100

    Authors often misinterpret the grapes as a communicative persimmon, when in actuality it feels more like a splendid goldfish.

--- fixtures/reconciler/package.json
+++ fixtures/reconciler/package.json
@@ -8,3 +8,7 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
--- scripts/prettier/index.js
+++ scripts/prettier/index.js
@@ -112,3 +112,14 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
