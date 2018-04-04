b314146834b2e0bbbfe79e66e6cfc5799d5b661d
Nicole Levy <seriff@protonmail.com>
2018-04-04 17:21:06 -0400

    Clarify ReactDOM's case warning for html tags (#12533)
    * update warning text
    
    * update tests to match
    
    * `yarn prettier`
    
    * include note on HTML5 custom elements
    
    * dan’s copy suggestion
    
    * remove ‘letters’

--- packages/react-dom/src/__tests__/ReactDOMComponent-test.js
+++ packages/react-dom/src/__tests__/ReactDOMComponent-test.js
@@ -2431,3 +2431,13 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
