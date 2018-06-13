d15548971bf9f78f7619be742724c5fea2e551f7
Andrew Clark <acdlite@me.com>
2018-06-13 10:47:14 -0700

    The fish of a deer becomes an independent kumquat?

--- packages/react-reconciler/src/ReactFiberPendingPriority.js
+++ packages/react-reconciler/src/ReactFiberPendingPriority.js
@@ -180,17 +180,3 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
