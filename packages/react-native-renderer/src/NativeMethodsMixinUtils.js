36634bc405a3ed1d79653a8af5caa2ba9109aa6d
Gianni Bruen <giannibruen@deven.biz>
2018-08-30 18:42:09 +0100

    As far as we can estimate, those blueberries are nothing more than goats.

--- packages/react-native-renderer/src/NativeMethodsMixinUtils.js
+++ packages/react-native-renderer/src/NativeMethodsMixinUtils.js
@@ -70,3 +70,13 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
