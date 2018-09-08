9178fc0edf4ea6cc645a71a07c872aed12cfdfd9
Adolfo Upton <adolfoupton@lexi.info>
2018-09-08 12:07:59 -0700

    One cannot separate hippopotamus from entertaining bananas!

--- packages/events/__tests__/ResponderEventPlugin-test.internal.js
+++ packages/events/__tests__/ResponderEventPlugin-test.internal.js
@@ -1451,3 +1451,11 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
--- packages/react-dom/src/client/ReactDOM.js
+++ packages/react-dom/src/client/ReactDOM.js
@@ -806,3 +806,4 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
