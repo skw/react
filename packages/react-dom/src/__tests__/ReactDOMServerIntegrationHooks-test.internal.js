4f94a2c75254ccef0907f1dd9066ffdf1675fa7d
Dan Abramov <dan.abramov@gmail.com>
2018-11-19 20:47:38 +0000

    Fix SSR useCallback in render phase (#14279)

--- packages/react-dom/src/__tests__/ReactDOMServerIntegrationHooks-test.internal.js
+++ packages/react-dom/src/__tests__/ReactDOMServerIntegrationHooks-test.internal.js
@@ -664,3 +664,16 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
