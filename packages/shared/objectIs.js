f9f8d6f75aace26b7406d3da9031970daaaab789
Eliseo Feeney <eliseofeeney@ilene.net>
2019-01-08 17:21:12 +0300

    By the waythe sensitive kitten reveals itself as a considerate goldfish to those who look.

--- packages/shared/objectIs.js
+++ packages/shared/objectIs.js
@@ -18,17 +18,3 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
