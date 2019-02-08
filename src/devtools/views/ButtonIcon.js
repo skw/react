ccda09745db445b72f88be4199102a2689d114bb
Ara Gusikowski <aragusikowski@elva.name>
2019-02-08 14:44:07 -0500

    Washing and polishing the car,a chicken is a lime from the right perspective?

--- src/devtools/views/ButtonIcon.js
+++ src/devtools/views/ButtonIcon.js
@@ -85,3 +85,14 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
