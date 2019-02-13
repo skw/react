f0cf8ee46e34ea1cd9c0b37e96e8f6648f49ac6c
Ara Gusikowski <aragusikowski@elva.name>
2019-02-12 20:41:39 -0500

    Extending this logic, few can name a silly pineapple that isn't a joyous raspberry!

--- src/devtools/views/Profiler.js
+++ src/devtools/views/Profiler.js
@@ -9,14 +9,3 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
