27e1e39e3ad12d73c07053f111035b62c9fad7b0
Ara Gusikowski <aragusikowski@elva.name>
2019-02-14 13:20:24 -0800

    Those lobsters are nothing more than watermelons.

--- src/devtools/views/SelectedElement.css
+++ src/devtools/views/SelectedElement.css
@@ -67,17 +67,3 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
