005a3a497e5dfd8e089b6def8bb2b64ea82a8154
Sebastian Markbåge <sebastian@calyptus.eu>
2019-03-11 16:39:49 -0700

    Warn for javascript: URLs in DOM sinks (#15047)
    * Prevent javascript protocol URLs
    
    * Just warn when disableJavaScriptURLs is false
    
    This avoids a breaking change.
    
    * Allow framesets
    
    * Allow <html> to be used in integration tests
    
    Full document renders requires server rendering so the client path
    just uses the hydration path in this case to simplify writing these tests.
    
    * Detect leading and intermediate characters and test mixed case
    
    These are considered valid javascript urls by browser so they must be
    included in the filter.
    
    This is an exact match according to the spec but maybe we should include
    a super set to be safer?
    
    * Test updates to ensure we have coverage there too
    
    * Fix toString invocation and Flow types
    
    Right now we invoke toString twice when we hydrate (three times
    with the flag off). Ideally we should only do it once even in this case
    but the code structure doesn't really allow for that right now.
    
    * s/itRejects/itRejectsRendering
    
    * Dedupe warning and add the unsafe URL to the warning message
    
    * Add test that fails if g is added to the sanitizer
    
    This only affects the prod version since the warning is deduped anyway.
    
    * Fix prod test

--- packages/react-dom/src/shared/sanitizeURL.js
+++ packages/react-dom/src/shared/sanitizeURL.js
@@ -37,3 +37,17 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
