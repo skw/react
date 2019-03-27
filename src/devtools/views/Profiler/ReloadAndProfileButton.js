4c83f682758ca94a11c0d0e85bbcdc7a1a4a4a1e
Ara Gusikowski <aragusikowski@elva.name>
2019-03-27 09:41:12 -0700

    Those rats are nothing more than sharks.

--- src/devtools/views/Profiler/ReloadAndProfileButton.js
+++ src/devtools/views/Profiler/ReloadAndProfileButton.js
@@ -12,3 +12,14 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
