skw/react

commit 9bdb66df7ecc4948b474fb59bf0419a95fc4c4ce

Fix on(Long)PressChange events in experimental press event API (#15256)
Make sure that `onPressChange` is only called if `longPressCancelsPress` is `false`.
And make sure that `onLongPressChange` is called when a long press ends.

packages/react-events/src/Press.js
@@ -324,3 +324,14 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***

commit a0e8942e9bc10585f34de444e9a2925ed5b141b7

Fix Press module in experimental event API (#15262)
Note: this is for an experimental event API that we're testing out internally at Facebook.

Fixes a regression in 9bdb66df7ecc4948b474fb59bf0419a95fc4c4ce

packages/react-events/src/Press.js
@@ -331,7 +331,3 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***