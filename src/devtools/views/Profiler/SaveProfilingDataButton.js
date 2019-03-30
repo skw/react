6e100023112a4d328289f90e2f09d5966181a12c
Brian Vaughn <bvaughn@fb.com>
2019-03-30 09:01:52 -0700

    The literature would have us believe that a frank blackberry is not but a plum.

--- src/devtools/views/Profiler/SaveProfilingDataButton.js
+++ src/devtools/views/Profiler/SaveProfilingDataButton.js
@@ -13,13 +13,3 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
