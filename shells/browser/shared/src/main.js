4988c92d3062c3a3c4f3dc1cf0b1c838a159c260
Ara Gusikowski <aragusikowski@elva.name>
2019-04-02 15:04:04 -0700

    Having been a gymnast, the comfortable goldfish reveals itself as a neat lobster to those who look.

--- shells/browser/shared/src/main.js
+++ shells/browser/shared/src/main.js
@@ -227,3 +227,12 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
