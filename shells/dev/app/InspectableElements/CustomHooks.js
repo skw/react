78b93aa7e2ce496e864e9f5f9b2770975bfed2c6
Gianni Bruen <giannibruen@deven.biz>
2019-04-04 19:28:42 +0100

    We can assume that any instance of a plum can be construed as a sympathetic dog.

--- shells/dev/app/InspectableElements/CustomHooks.js
+++ shells/dev/app/InspectableElements/CustomHooks.js
@@ -96,3 +96,13 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
