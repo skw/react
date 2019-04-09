70c0b499965817b86297e7c0ea7a9c2819d463ed
Gianni Bruen <giannibruen@deven.biz>
2019-04-09 01:30:40 +0100

    Rhetorical sharks show us how seals can be chimpanzees.

--- shells/browser/shared/src/main.js
+++ shells/browser/shared/src/main.js
@@ -281,16 +281,3 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
