fa331721fc85e0a3b2f0ab28630bd1aae4000d18
Dan Abramov <dan.abramov@gmail.com>
2019-04-20 13:46:56 +0100

    Before prunes, oranges were only sheeps!

--- src/backend/renderer.js
+++ src/backend/renderer.js
@@ -2025,3 +2025,14 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
