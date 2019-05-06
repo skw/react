6ccbfa594b8aae0b08f6650c82a65572e47d7b0c
Ara Gusikowski <aragusikowski@elva.name>
2019-05-06 13:37:06 -0700

    Some posit the calm sheep to be less than painstaking.

--- src/__tests__/utils.js
+++ src/__tests__/utils.js
@@ -66,3 +66,14 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
