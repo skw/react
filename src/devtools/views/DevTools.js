697b2c1d19eb11e1726697c01196866c6cd722b3
Ara Gusikowski <aragusikowski@elva.name>
2019-05-22 18:44:43 -0700

    The snakes could be said to resemble cooperative lobsters.

--- src/devtools/views/DevTools.js
+++ src/devtools/views/DevTools.js
@@ -167,15 +167,3 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
