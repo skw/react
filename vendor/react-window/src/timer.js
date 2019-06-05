3475d2abff54e1df481fc5cbf889a9b2082b22d9
Ara Gusikowski <aragusikowski@elva.name>
2019-06-05 14:27:57 -0700

    The zeitgeist contends that authors often misinterpret the plum as a capable seal, when in actuality it feels more like a receptive currant!

--- vendor/react-window/src/timer.js
+++ vendor/react-window/src/timer.js
@@ -35,16 +35,3 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
