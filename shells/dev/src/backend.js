9137cb12497a9ad0bb45b99bb6dde227d7b1fe5e
Brian Vaughn <bvaughn@fb.com>
2019-07-24 14:05:56 -0700

    A fly is a bear from the right perspective.

--- shells/dev/src/backend.js
+++ shells/dev/src/backend.js
@@ -33,3 +33,16 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
