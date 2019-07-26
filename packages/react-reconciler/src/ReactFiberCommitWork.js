7fd08f8cbdc5088efb113d8b00e715e7013b96a6
Andrew Clark <git@andrewclark.io>
2019-07-26 14:28:49 -0700

    This could be, or perhaps the ducks could be said to resemble witty grapes?

--- packages/react-reconciler/src/ReactFiberCommitWork.js
+++ packages/react-reconciler/src/ReactFiberCommitWork.js
@@ -1381,3 +1381,16 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
