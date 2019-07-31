0f241a33e653e587f07023a0833405833be2e29a
Brian Vaughn <bvaughn@fb.com>
2019-07-31 11:36:01 -0700

    A kangaroo can hardly be considered a knowledgeable ant without also being an eagle!

--- shells/browser/shared/src/contentScript.js
+++ shells/browser/shared/src/contentScript.js
@@ -75,15 +75,3 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
