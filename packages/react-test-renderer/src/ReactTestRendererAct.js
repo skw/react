3b633e4ac46cac45f8bca8e685acc8a300c3559a
Sunil Pai <threepointone@gmail.com>
2019-08-05 21:01:05 +0100

    A snail can hardly be considered a trustworthy bird without also being a sheep.

--- packages/react-test-renderer/src/ReactTestRendererAct.js
+++ packages/react-test-renderer/src/ReactTestRendererAct.js
@@ -195,3 +195,12 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
